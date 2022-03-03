19f6cf286cd5a5bdd783e9f827e8794038e1a7d6
ermias-mangisto <ermiasmangisto@gmail.com>
2022-03-03 15:10:25 +0200

    .

--- Server/server.js
+++ Server/server.js
@@ -19,8 +19,8 @@ app.use('/register',UserRouter);
 app.use('/employee',passport.authenticate("jwt",{session:false}),EmployeeRouter);
 
 if(process.env.NODE_ENV == 'production'){
-    app.use(express.static(path.resolve(__dirname,"../client/build","index.html")));
+    app.use(express.static(path.join(__dirname,"../client/build","index.html")));
     app.get("*",(req, res) =>{
-        res.sendFile(path.resolve(__dirname,"../client/build","index.html"))
+        res.sendFile(path.join(__dirname,"../client/build","index.html"))
     })
 }
